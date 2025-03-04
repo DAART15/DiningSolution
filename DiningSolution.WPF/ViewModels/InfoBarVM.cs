030bb08da18f7ba35e6f99596935b2e2bbc162b9
ramunasdapkus@gmail.com <ramunasdapkus@gmail.com>
2025-03-04 16:54:35 +0200

    worked on InfoBar Date Time

--- DiningSolution.WPF/ViewModels/InfoBarVM.cs
+++ DiningSolution.WPF/ViewModels/InfoBarVM.cs
@@ -1,14 +1,38 @@
 ﻿using DiningSolution.WPF.Commands;
 using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace DiningSolution.WPF.ViewModels
 {
     public class InfoBarVM : INotifyPropertyChanged
     {
         public CloseAppCommand CloseAppCommand { get; set; }
+        private string _date;
+
+        public string Date
+        {
+            get { return _date; }
+            set 
+            { 
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+        private string _time;
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged(nameof(Time));
+            }
+        }
+
         public InfoBarVM()
         {
             CloseAppCommand = new CloseAppCommand(this);
+            StartClock();
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -19,6 +43,19 @@ namespace DiningSolution.WPF.ViewModels
         public void CloseApp()
         {
             App.Current.Shutdown();
+        }
+        private void StartClock()
+        {
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += (sender, e) =>
+            {
+                Time = DateTime.Now.ToString("HH:mm:ss");
+                Date = DateTime.Now.ToString("yyyy-MM-dd");
+            };
+            timer.Start();
         }
     }
 }
--- ToDo_Tasks.cs
+++ ToDo_Tasks.cs
@@ -5,9 +5,12 @@ public class ToDoTasks
 	public ToDoTasks()
 	{
     }
-    
+
 
     // WPF Style pasidaryti pagal kalkuliatoriaus pvz
+
+    // WPF InfoBar > laikrodis
+    // WPF LoginLangas> kurti
 
     // WPF MainWindow.xaml > Vietoje btn padaryti binding i kitus langus kai jie jau bus sukurti
 
